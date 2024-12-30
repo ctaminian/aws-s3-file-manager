1eebd5f5ae6f600bbc9772dae1b5d482074089d3
ctaminian <ctaminian@gmail.com>
2024-12-29 20:32:55 -0800

    Add function to get files from the bucket.

--- s3_manager.py
+++ s3_manager.py
@@ -1,7 +1,6 @@
 import boto3
 from dotenv import load_dotenv
 import os
-from botocore.exceptions import NoCredentialsError, PartialCredentialsError, ClientError
 
 load_dotenv()
 
@@ -9,15 +8,24 @@ aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
 aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
 aws_region = os.getenv("AWS_REGION")
 
-try:
-    s3_client = boto3.client("s3", aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key, region_name=aws_region)
-    print("Connected to S3 successfully!")
+s3 = boto3.client("s3", aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key, region_name=aws_region)
+print("Connected to S3 successfully!")
 
-except NoCredentialsError:
-    print("Error: No credentials provided. Check your .env file.")
-except PartialCredentialsError:
-    print("Error: Incomplete credentials provided. Verify your .env file.")
-except ClientError as e:
-    print(f"Client error: {e}")
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
+bucket_name = "python-s3-demo-bucket"
+
+def get_file_list():
+    try:
+        file_list = s3.list_objects_v2(Bucket=bucket_name)
+        if "Contents" not in file_list:
+            print("The bucket is empty.")
+            return
+        
+        print(f"You have {file_list['KeyCount']} files on this bucket:")
+
+        for i, file in enumerate(file_list["Contents"], start=1):
+            print(f"{i}. {file['Key']}")
+
+    except Exception as e:
+        print(f"An error occured: {e}")
+        
+get_file_list()
