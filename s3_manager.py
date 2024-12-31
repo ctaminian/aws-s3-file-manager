21122889684344c40fd906b69458901c836bd686
ctaminian <ctaminian@gmail.com>
2024-12-30 20:02:01 -0800

    Add function to download files from the s3 bucket.

--- s3_manager.py
+++ s3_manager.py
@@ -15,9 +15,10 @@ def main():
     print("Connected to S3 successfully!")
 
     bucket_name = "python-s3-demo-bucket"
-    #get_file_list(s3, bucket_name)
 
+    #get_file_list(s3, bucket_name)
     #upload_file(s3, "C:/Users/ctami/OneDrive/Desktop/file2.txt", bucket_name, "file2.txt")
+    #download_file(s3, bucket_name, "file2.txt", "C:/Users/ctami/OneDrive/Desktop/test")
 
 def get_file_list(s3, bucket_name):
     try:
@@ -50,6 +51,30 @@ def upload_file(s3, file_path, bucket_name, file_name):
         try:
             s3.upload_file(file_path, bucket_name, file_name)
             print(f"File '{file_name}' uploaded to bucket '{bucket_name}' successfully.")
+            return True
+        except ClientError as e:
+            print(f"Upload failed: {e.response['Error']['Message']}")
+            return False
+        except Exception as e:
+            print(f"An unexpected error occurred during upload: {e}")
+            return False
+
+def download_file(s3, bucket_name, file_name, file_path=None):
+    if file_path is None:
+        file_path = os.path.join(os.getcwd(), file_name)
+    else:
+        if os.path.isdir(file_path):
+            file_path = os.path.join(file_path, file_name)
+
+    if not isinstance(bucket_name, str) or not bucket_name.strip():
+        raise ValueError("Please specifity a bucket name.")
+    if not isinstance(file_name, str) or not file_name.strip():
+        raise ValueError("Please specifity a file name.")
+
+    else:
+        try:
+            s3.download_file(bucket_name, file_name, file_path)
+            print(f"File '{file_name}' downloaded to '{file_path}' successfully.")
             return True
         except ClientError as e:
             print(f"Upload failed: {e.response['Error']['Message']}")
