263c07d24fe5abcfbaf6912f9d282063c8440f62
ctaminian <ctaminian@gmail.com>
2025-02-04 13:11:58 -0800

    Add frame for buttons and declare output textbox before list files function.

--- s3_manager.py
+++ s3_manager.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from botocore.exceptions import ClientError
 
 def main():
-
+    
     # Load environment variables
     load_dotenv()
 
@@ -31,35 +31,38 @@ def main():
     customtkinter.set_appearance_mode("Dark")
     app = customtkinter.CTk()
     app.title("AWS S3 Manager")
-    app.geometry("600x800")
+    app.geometry("1000x900")
 
     frame = customtkinter.CTkFrame(app)
     frame.pack(padx=20, pady=20, fill="both", expand=True)
 
-    title_label = customtkinter.CTkLabel(frame, text="AWS S3 File Manager", font=("Helvetica", 20))
+    top_frame = customtkinter.CTkFrame(frame, fg_color="#242424")
+    top_frame.pack(padx=20, pady=20)
+
+    title_label = customtkinter.CTkLabel(top_frame, text="AWS S3 File Manager", font=("Helvetica", 20))
     title_label.pack(padx=20, pady=10)
 
-    usage_label = customtkinter.CTkLabel(frame, text="Please click on any of the buttons below to begin.", font=("Helvetica", 12))
+    usage_label = customtkinter.CTkLabel(top_frame, text="Please click on any of the buttons below to begin.", font=("Helvetica", 14))
     usage_label.pack(padx=20, pady=10)
 
-    list_button = customtkinter.CTkButton(frame,text="List Files", fg_color="#248823", hover_color="#014422", command=lambda: list_files(s3, bucket_name, output_textbox))
-    list_button.pack(padx=20, pady=10)
-
-    upload_button = customtkinter.CTkButton(frame,text="Upload File", fg_color="#248823", hover_color="#014422")
-    upload_button.pack(padx=20, pady=10)
-
-    download_button = customtkinter.CTkButton(frame,text="Download File", fg_color="#248823", hover_color="#014422")
-    download_button.pack(padx=20, pady=10)
-
-    delete_button = customtkinter.CTkButton(frame,text="Delete File", fg_color="#248823", hover_color="#014422")
-    delete_button.pack(padx=20, pady=10)
-
-    exit_button = customtkinter.CTkButton(frame,text="Exit", fg_color="#248823", hover_color="#014422", command=exit_program)
-    exit_button.pack(padx=20, pady=10)
-
-    output_textbox = customtkinter.CTkTextbox(frame, width=600, height=800, border_spacing=10)
+    output_textbox = customtkinter.CTkTextbox(frame, width=900, height=900, border_spacing=20, font=("Helvetica", 14), fg_color="#1d1e1e")
     output_textbox.pack(padx=20, pady=20)
-    
+
+    list_button = customtkinter.CTkButton(top_frame, text="List Files", fg_color="#248823", hover_color="#014422", command=lambda: list_files(s3, bucket_name, output_textbox))
+    list_button.pack(side="left", padx=20, pady=20)
+
+    upload_button = customtkinter.CTkButton(top_frame, text="Upload File", fg_color="#248823", hover_color="#014422")
+    upload_button.pack(side="left", padx=20, pady=20)
+
+    download_button = customtkinter.CTkButton(top_frame, text="Download File", fg_color="#248823", hover_color="#014422")
+    download_button.pack(side="left", padx=20, pady=20)
+
+    delete_button = customtkinter.CTkButton(top_frame, text="Delete File", fg_color="#248823", hover_color="#014422")
+    delete_button.pack(side="left", padx=20, pady=20)
+
+    exit_button = customtkinter.CTkButton(top_frame, text="Exit", fg_color="#248823", hover_color="#014422", command=exit_program)
+    exit_button.pack(side="left", padx=20, pady=20)
+
     app.mainloop()
 
 def exit_program():
@@ -67,13 +70,14 @@ def exit_program():
     sys.exit()
 
 def list_files(s3, bucket_name, output_textbox):
-    output_textbox.insert("end","You chose to list files in the bucket\n")
-    output_textbox.insert("end","Fetching files...\n")
+    output_textbox.insert("end", "You chose to list files in the bucket\n")
+    output_textbox.insert("end", "Fetching files...\n")
+    output_textbox.insert("end", "\n")
     try:
         validate_inputs(bucket_name, "bucket name")
         file_list = s3.list_objects_v2(Bucket=bucket_name)
         if "Contents" not in file_list:
-            output_textbox.insert("end","The bucket is empty.\n")
+            output_textbox.insert("end", "The bucket is empty.\n")
             return
         output_textbox.insert("end", f"You have {file_list['KeyCount']} files on this bucket:\n")
         for i, file in enumerate(file_list["Contents"], start=1):
@@ -91,12 +95,12 @@ def verify_bucket(s3, bucket_name):
     except ClientError as e:
         print(f"Error verifying bucket: {e.response['Error']['Message']}")
         return False
-    
+
 # Validate string inputs
 def validate_inputs(value, name):
     if not isinstance(value, str) or not value.strip():
         raise ValueError(f"Please specify a valid {name}")
-    
+
 # Upload file. S3 client, file path, bucket and file name required
 def upload_file(s3, file_path, bucket_name, file_name):
     validate_inputs(bucket_name, "bucket name")
@@ -114,8 +118,8 @@ def upload_file(s3, file_path, bucket_name, file_name):
     except Exception as e:
         print(f"An unexpected error occurred during the file upload: {e}")
         return False
-    
-# Download file. S3 client, bucket, file name required, destination path is optional, if left blank file downloads to current directory
+
+# Download file. S3 client, bucket, file name required, destination path is optional
 def download_file(s3, bucket_name, file_name, destination_path=None):
     validate_inputs(bucket_name, "bucket name")
     validate_inputs(file_name, "file name")
